2202eaacf89bd7c36f444871b0a2963206dc7766
HedgehogInTheCPP <a.rainman@gmail.com>
2020-06-15 14:41:05 +0300

    Create Aliexpress mobile to desktop redirector

---
+++ Aliexpress mobile to desktop.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Aliexpress mobile to desktop version redirector
+// @homepageURL https://github.com/IRainman/user_scripts
+// @namespace   https://github.com/IRainman/user_scripts
+// @supportURL  https://github.com/IRainman/user_scripts/issues
+// @version     1.0
+// @description Automatically redirects from mobile to desktop version
+// @author      HedgehogInTheCPP
+// @match       https://m.aliexpress.com/*
+// @match       http://m.aliexpress.com/*
+// @grant       none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let url = 'https://aliexpress.com' + window.location.href.toString().split(window.location.host)[1];
+    let form = document.createElement('form');
+    form.method = 'post';
+    form.action = url;
+    document.body.appendChild(form);
+    form.submit();
+})();
